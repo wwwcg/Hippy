125ec3e6b89524daa4188a9b8fddea3e883c5428
mengyanluo <mengyanluo@tencent.com>
2021-04-08 22:56:03 +0800

    chore: release 2.3.4.13w

--- ios/sdk/base/HippyBridge.mm
+++ ios/sdk/base/HippyBridge.mm
@@ -42,7 +42,7 @@ NSString *const HippyJavaScriptDidLoadNotification = @"HippyJavaScriptDidLoadNot
 NSString *const HippyJavaScriptDidFailToLoadNotification = @"HippyJavaScriptDidFailToLoadNotification";
 NSString *const HippyDidInitializeModuleNotification = @"HippyDidInitializeModuleNotification";
 NSString *const HippyBusinessDidLoadNotification = @"HippyBusinessDidLoadNotification";
-NSString *const _HippySDKVersion = @"2.3.4.12w";
+NSString *const _HippySDKVersion = @"2.3.4.13w";
 
 static NSMutableArray<Class> *HippyModuleClasses;
 NSArray<Class> *HippyGetModuleClasses(void)
