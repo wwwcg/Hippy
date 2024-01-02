801ac17f982c7b8469cfe5e4503d35d31b05c5ed
ilikethese <shenli0124@qq.com>
2024-01-02 14:21:05 +0800

    feat(hermes): use std::any_cast in ios 11 when opening c++ exception

--- framework/ios/base/executors/HippyJSExecutor.mm
+++ framework/ios/base/executors/HippyJSExecutor.mm
@@ -415,7 +415,9 @@ constexpr char kHippyGetTurboModule[] = "getTurboModule";
             for (size_t i = 0; i < info.Length(); ++i) {
                 argv.push_back(info[i]);
             }
-            auto scope_wrapper = reinterpret_cast<hippy::ScopeWrapper*>(std::any_cast<void*>(info.GetSlot()));
+        std::any slot_any = info.GetSlot();
+        auto any_pointer = std::any_cast<void*>(&slot_any);
+        auto scope_wrapper = reinterpret_cast<hippy::ScopeWrapper*>(static_cast<void *>(*any_pointer));
             auto scope = scope_wrapper->scope.lock();
             FOOTSTONE_CHECK(scope);
             auto turbo_wrapper = reinterpret_cast<TurboWrapper*>(data);
@@ -425,7 +427,9 @@ constexpr char kHippyGetTurboModule[] = "getTurboModule";
             info.GetReturnValue()->Set(result);
         }, turbo_wrapper.get());
         [turbo saveTurboWrapper:name turbo:std::move(turbo_wrapper)];
-        auto scope_wrapper = reinterpret_cast<hippy::ScopeWrapper*>(std::any_cast<void*>(info.GetSlot()));
+      std::any slot_any = info.GetSlot();
+      auto any_pointer = std::any_cast<void*>(&slot_any);
+      auto scope_wrapper = reinterpret_cast<hippy::ScopeWrapper*>(static_cast<void *>(*any_pointer));
         auto scope = scope_wrapper->scope.lock();
         FOOTSTONE_CHECK(scope);
         auto func = scope->GetContext()->CreateFunction(func_wrapper);
