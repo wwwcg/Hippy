4536cb4c6fdff053d68b1ed7bd7b4152dd3c0db8
wwwcg <isarraypro@gmail.com>
2024-08-05 14:20:10 +0800

    fix(ios): eliminate all warnings and open treat_warning_as_error

--- modules/ios/image/HippyImageProviderProtocol.h
+++ modules/ios/image/HippyImageProviderProtocol.h
@@ -24,6 +24,8 @@
 
 @class UIImage;
 
+NS_ASSUME_NONNULL_BEGIN
+
 @protocol HippyImageProviderProtocol <NSObject>
 
 @required
@@ -86,3 +88,5 @@
 - (NSTimeInterval)delayTimeAtFrame:(NSUInteger)frame;
 
 @end
+
+NS_ASSUME_NONNULL_END
